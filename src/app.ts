de5f1da6fc1d7669688c5f93b8a8cc9f5cf8c4f3
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2023-11-17 17:14:22 +0600

    ff

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 
@@ -7,10 +7,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
-
-// console.log(process.cwd());
 
 export default app;
